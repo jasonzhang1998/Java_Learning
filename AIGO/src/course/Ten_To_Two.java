9d61cf2b1d4dc928636c63d80d9f72e6c25cc393
Jasonzhang1998 <jasonzhang176301@gmail.com>
2021-04-16 23:38:10 +0800

    2021.4.16 update

---
+++ AIGO/src/course/Ten_To_Two.java
@@ -0,0 +1,27 @@
+package course;
+
+import java.util.Stack;
+
+public class Ten_To_Two {
+    //十进制转二进制
+    public static void main(String[] args) {
+        System.out.println(solution_1(9));
+        System.out.println("hello");
+    }
+
+    //使用一个栈，存放每次对2取余后的结果，最后出栈即为所求
+    public static String solution_1(int x) {
+        int res;
+        Stack num = new Stack();
+        while (x != 0) {
+            res = x % 2;
+            num.push(res);
+            x = x / 2;
+        }
+        String s = "";
+        while (!num.empty()) {
+            s = s.concat(num.pop().toString());
+        }
+        return s;
+    }
+}
